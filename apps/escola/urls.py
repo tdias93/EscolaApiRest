2e7f83481083f447417cb3c888f2e91cb5d3e8e1
Thiago Dias Olimpio <thiago.olimpio93@gmail.com>
2024-03-21 16:56:28 -0300

    Apontamento de url

--- apps/escola/urls.py
+++ apps/escola/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from apps.escola.views import alunos
+from django.urls import path, include
+from apps.escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet
+from rest_framework import routers
+
+
+routers = routers.DefaultRouter()
+routers.register('alunos', AlunosViewSet, basename='Alunos')
+routers.register('cursos', CursosViewSet, basename='Cursos')
+routers.register('matricula', MatriculaViewSet, basename='Matricula')
 
 urlpatterns = [
-    path('alunos/', alunos),
+    path('', include(routers.urls)),
 ]
