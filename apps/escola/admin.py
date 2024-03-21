923e1bc894ce7aec5cf051930637db80d8525d67
Thiago Dias Olimpio <thiago.olimpio93@gmail.com>
2024-03-21 16:55:08 -0300

    Modelo do Admin

--- apps/escola/admin.py
+++ apps/escola/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from apps.escola.models import Aluno, Curso, Matricula
 
-# Register your models here.
+class AlunosAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Aluno, AlunosAdmin)
+
+
+class CursosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',) 
+
+admin.site.register(Curso, CursosAdmin)
+
+
+class MatriculaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'aluno', 'curso', 'periodo')
+    list_display_links = ('id',)
+
+admin.site.register(Matricula, MatriculaAdmin)
